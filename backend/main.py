70e2a7c171977459f975184d4bd59fc0bf89cbb4
bikrambohara <bikram.bohara023@apexcollege.edu.np>
2026-08-16 11:58:57 +0545

    complete the student croud opetration

--- backend/main.py
+++ backend/main.py
@@ -45,3 +45,51 @@ def create_student(student:schemas.StudentCreate, db:Session=Depends(get_db)):
 
 # read all students
 
+@app.get("/students", response_model = list[schemas.Student])
+def get_students(db:Session=Depends(get_db)):
+    return db.query(models.Student).all()
+
+# red one student by id
+
+@app.get("/students/{student_id}", response_model=schemas.Student)
+def get_student_by_id(student_id:int, db:Session=Depends(get_db)):
+    student = db.query(models.Student).filter(models.Student.id == student_id).first()
+    if not student:
+        raise HTTPException(status_code=404, detail="Student not found")
+    return student
+
+
+# update student
+@app.put("/students/{student_id}", response_model=schemas.Student)
+def update_student(student_id:int, student:schemas.StudentUpdate, db:Session=Depends(get_db)):
+    existing_student = db.query(models.Student).filter(models.Student.id == student_id).first()
+    if not existing_student:
+        raise HTTPException(status_code=404, detail="Student not found")
+
+    existing_student.name = student.name
+    existing_student.email = student.email
+    existing_student.phone = student.phone
+    existing_student.address = student.address
+    existing_student.date_of_birth = student.date_of_birth
+    existing_student.gender = student.gender
+    existing_student.semester = student.semester
+    existing_student.program = student.program
+    existing_student.enrollment_date = student.enrollment_date
+    existing_student.status = student.status
+
+    db.commit()
+    db.refresh(existing_student)
+    return existing_student
+
+
+# delete student
+@app.delete("/students/{student_id}", response_model=schemas.Student)
+def delete_student(student_id:int, db:Session=Depends(get_db)):
+    student = db.query(models.Student).filter(models.Student.id == student_id)
+    if not student.first():
+        raise HTTPException(status_code=404, detail="Student not found")
+
+    student.delete()
+    db.commit()
+
+    return {"message": "Student deleted successfuly"}
--- backend/schemas.py
+++ backend/schemas.py
@@ -15,6 +15,20 @@ class StudentCreate(BaseModel):
     status: str = "Active"
 
 
+# Update schema
+class StudentUpdate(BaseModel):
+    name: str
+    email: str
+    phone: str
+    address: str
+    date_of_birth: str
+    gender: str
+    semester: int
+    program: str
+    enrollment_date: str
+    status: str
+
+
 # Output schema
 class Student(BaseModel):
     id: int
